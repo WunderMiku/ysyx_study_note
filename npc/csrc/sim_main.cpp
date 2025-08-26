40e76323cc7deeb94393f945856826924bbd7c57
wundermiku <wundermiku@outlook.com>
2025-08-26 17:55:43 +0800

    2025_8_26_Update

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -1,31 +1,48 @@
 #include "Vtop.h"
 #include "verilated.h"
+#include "verilated_fst_c.h"
 
 #define MAX_SIM_TIME 50
 
 vluint64_t sim_time = 0;
 
-VerilatedContext* contextp = new VerilatedContext;
 
-Vtop* top = new Vtop{contextp};
-int get_truetable(int a, int b) {
+int get_truetable(Vtop* top, int a, int b);
+int main(int argc, char** argv) {
+	const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
+	const std::unique_ptr<Vtop> top{new Vtop{contextp.get(), "TOP"}};
+
+	if (false && argc && argv) {} // Prevent unused variable warnings
+	Verilated::mkdir("logs"); // Create a directory for logs
+	contextp->debug(0);
+	contextp->traceEverOn(true); // Enable tracing
+	contextp->commandArgs(argc, argv);
+
+	VerilatedFstC* tfp = new VerilatedFstC;
+	top->trace(tfp, 5);  // 设置跟踪深度
+	tfp->open("logs/sim.fst");  // 打开并创建波形文件
+
+	
+	for (int a = 0; a < 2; a++) {
+        for (int b = 0; b < 2; b++) {
+			int result = get_truetable(top.get(), a, b);
+			printf("a: %d, b: %d, f: %d\n", a, b, result);
+			contextp->timeInc(1);
+			tfp->dump(contextp->time());
+		}
+	}
+	
+	// 额外的时间步进以确保所有数据都被展示
+	contextp->timeInc(1);
+	tfp->dump(contextp->time());
+	top->final();
+	tfp->close();
+	return 0;
+}
+
+int get_truetable(Vtop* top, int a, int b) {
 	top->a = a;
 	top->b = b;
 	top->eval();
 	return top->f;
-}
-int main(int argc, char** argv) {
-	contextp->commandArgs(argc, argv);
-	
-	
-	for (int a = 0; a < 2; a++) {
-        for (int b = 0; b < 2; b++) {
-			int result = get_truetable(a, b);
-			printf("a: %d, b: %d, f: %d\n", a, b, result);
-		}
-	}
-	
-	delete top;
-	delete contextp;
-	return 0;
 }
