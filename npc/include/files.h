08b36b23129a620a55a16024305608b4ed665031
SunHaotian <wundermiku@outlook.com>
2025-12-05 21:31:52 +0800

    OSCPU/chisel-playground master initialized
    OSCPU/chisel-playground 08af0f9dd3307ae7d51941bf5cce0671bf19f9c0 Merge pull request #12 from zlh20040308/fix-name

--- npc/include/files.h
+++
@@ -1,7 +0,0 @@
-#ifndef __FILES_H__
-#define __FILES_H__
-
-#include <stdint.h>
-uint32_t loadFile(int argc, char** argv);
-
-#endif
--- npc/include/sdb/sdb.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SDB_H__
-#define __SDB_H__ 
-
-#include <stdint.h>
-void sdbMainLoop();
-uint32_t expr(char *e, bool *success);
-void init_regex();
-
-#endif
